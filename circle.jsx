4308e6b1350010ba6bf25fa7f33ceb97ca053f54
MUHAMMAD SHAHZAIB <126311454+muhammad-shahzab@users.noreply.github.com>
2024-09-09 04:58:01 +0500

    RU_Component ID1
    a circle with 2 bootom 1 top image

---
+++ circle.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+function circle() {
+  return (
+    <div>
+      
+      <div className='HP-circular-content-container'>
+
+<div className='HP-circular-content-circle'>
+
+
+    <div className='HP-circularBanner-imgTOP picture-container1'>
+        <img src={HPimages.roboAlpha} alt="temp" />
+    </div>
+
+    <div className='HP-circularBanner-text'>
+
+        <h1>Give your <br></br><span >Threads </span>To <br></br>JERBOAS</h1>
+
+        <p>Just put a neche Jerboas will ceate your content and publish them.
+            <br /> with super Ai Assistance Now automate your month
+        </p>
+
+    </div>
+    <div className='HP-circularBanner-imgBottom '>
+        <div className='picture-container1'>
+            <img src={HPimages.roboGamma} alt="temp" />
+        </div>
+        <div className='picture-container1'>
+            <img src={HPimages.roboBeta} alt="temp" />
+        </div>
+    </div>
+
+
+</div>
+</div>
+
+<div class="HP-circularBanner-para small-screen-background overlay-boxes-enhanced">
+<p>Just put a neche Jerboas will ceate your content and publish them.<br />
+    with super Ai Assistance Now automate your month
+</p>
+</div>
+    </div>
+  )
+}
+
+export default circle
